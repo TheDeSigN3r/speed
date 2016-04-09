2ae022faf7feec684246b710e619b62cfa74d39c
aliafshar250 <mehdizare800@mailfa.com>
2016-04-09 20:06:07 +0430

    Update lock_pars.lua

--- plugins/lock_pars.lua
+++ plugins/lock_pars.lua
@@ -1,52 +1,63 @@
-local function run(msg)
-
+local function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_pars'] == 'yes' then
-
-
-if not is_momod(msg) then
-
-
-chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msg = 'You cannot use PARS words here . Bye ! '
-   local receiver = msg.to.id
-    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
-
-      end
-   end
-
-
-return {
-patterns = {
-"[ص](.*)",
-"[ث](.*)",
-"[ق](.*)",
-"[ف](.*)",
-"[ع](.*)",
-"[ه](.*)",
-"[خ](.*)",
-"[ح](.*)",
-"[ج](.*)",
-"[چ](.*)",
-"[ش](.*)",
-"[س](.*)",
-"[ی](.*)",
-"[ب](.*)",
-"[ل](.*)",
-"[ا](.*)",
-"[ت](.*)",
-"[ن](.*)",
-"[م](.*)",
-"[پ](.*)",
-"[ط](.*)",
-"[ز](.*)",
-"[ر](.*)",
-"[د](.*)",
-"[و](.*)",
-"[ک](.*)",
-"[گ](.*)",
-}, 
-run = run
-}
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_emoji'] then
+                lock_emoji = data[tostring(msg.to.id)]['settings']['lock_emoji']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_emoji == "yes" then
+        send_large_msg(chat, 'به دلیل  ارسال اموجی از گروه حذف شدید')
+        chat_del_user(chat, user, ok_cb, true)
+    end
 end
+ 
+return {
+  patterns = {
+    "(ص)",
+    "(ث)",
+    "(ق)",
+    "(ف)",
+    "(ع)",
+    "(غ)",
+    "(ه)",
+    "(خ)",
+    "(ح)",
+    "(ج)",
+    "(چ)",
+    "(ش)",
+    "(س)",
+    "(ی)",
+    "(ب)",
+    "(ل)",
+    "(ا)",
+    "(ت)",
+    "(ن)",
+    "(م)",
+    "(پ)",
+    "(ط)",
+    "(ز)",
+    "(ر)",
+    "(ژ)",
+    "(د)",
+    "(ذ)",
+    "(و)",
+    "(ک)",
+    "(گ)",
+    "(ء)",
+    "(ئ)",
+    "(ظ)",
+  },
+  run = run
+}
+--Copyright and edit; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است--
+ 

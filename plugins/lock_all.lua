aa0409604e210c3d9cf5d4a1e514ce2866cad881
aliafshar250 <mehdizare800@mailfa.com>
2016-04-09 20:27:33 +0430

    Update lock_all.lua

--- plugins/lock_all.lua
+++ plugins/lock_all.lua
@@ -1,52 +1,57 @@
-local function run(msg)
-
+local function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_all'] == 'yes' then
-
-
-if not is_momod(msg) then
-
-
-chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msg = 'به دلیل استفاده از دستورات ربات از گروه حذف شدید'
-   local receiver = msg.to.id
-    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
-
-      end
-   end
-
-
-return {
-patterns = {
-"(lock link)",
-"(lock emoji)",
-"(settings)",
-"(id)",
-"(lock member)",
-"(newlink)",
-"(lock)",
-"(link)",
-"(unlock)",
-"(linkpv)",
-"(help)",
-"(teleseed)",
-"(kick)",
-"(ban)",
-"(unban)",
-"(set about)",
-"(setrules)",
-"(owner)",
-"(res)",
-"(info)",
-"(who)",
-"(modlist)",
-"(kickme)",
-"(promote)",
-"(demote)",
-"(about)",
-"(aetphoto)",
-}, 
-run = run
-}
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_all'] then
+                lock_all = data[tostring(msg.to.id)]['settings']['lock_all']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_emoji == "yes" then
+        send_large_msg(chat, 'به دلیل استفاده از دستورات حذف شدید')
+        chat_del_user(chat, user, ok_cb, true)
+    end
 end
+ 
+return {
+  patterns = {
+    "(lock link)",
+    "(lock emoji)",
+    "(settings)",
+    "(id)",
+    "(lock member)",
+    "(newlink)",
+    "(lock)",
+    "(link)",
+    "(unlock)",
+    "(linkpv)",
+    "(help)",
+    "(teleseed)",
+    "(kick)",
+    "(ban)",
+    "(unban)",
+    "(set about)",
+    "(setrules)",
+    "(owner)",
+    "(res)",
+    "(info)",
+    "(who)",
+    "(modlist)",
+    "(kickme)",
+    "(promote)",
+    "(demote)",
+    "(about)",
+    "(setphoto)",
+  },
+  run = run
+}
+--Copyright and edit; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است--
+ 
